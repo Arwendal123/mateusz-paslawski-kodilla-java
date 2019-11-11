902d3ef354b365b7fc0dc9ef958631d0c054a997
Arwendal123 <mateuszpaslawski@gmail.com>
2019-11-11 17:23:06 +0100

    7.5.1

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -8,6 +8,18 @@ public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage() {
         //Given
+        int[] numbers = {-2, -1, 0, 1, 2, 3, 4, 567};
+
+        //When
+        double result = ArrayOperations.getAverage(numbers);
+
+        //Then
+        Assert.assertEquals(71.75, result, 0);
+    }
+
+    @Test
+    public void testGetAverageIfListEmpty() {
+        //Given
         int[] numbers = {};
 
         //When
