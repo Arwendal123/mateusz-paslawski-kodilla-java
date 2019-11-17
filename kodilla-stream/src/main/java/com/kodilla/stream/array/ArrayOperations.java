279e2153cb060863b31c7504ade6f19eaa35f7f1
Arwendal123 <mateuszpaslawski@gmail.com>
2019-11-17 13:38:31 +0100

    7.5.1

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,12 +4,8 @@ import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers){
-        if(numbers.length != 0) {
-            return IntStream.range(0, numbers.length)
-                    .map(n -> numbers[n])
-                    .average().orElse(0);
-        }else{
-            return 0.0;
-        }
+        return IntStream.range(0, numbers.length)
+              .map(n -> numbers[n])
+              .average().orElse(0);
     }
 }
